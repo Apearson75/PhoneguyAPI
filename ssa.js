19ff0995ebf451d443a0068d6e31ba1bb0a97a58
Phoneguytech75 <shahidhusaindada@outlook.com>
2021-09-07 21:04:52 +0100

    ssa

---
+++ ssa.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const router = express.Router()
+
+module.exports = router
+
+router.get('/', (req,res) => {
+   res.send({
+       working: 'true'
+   })
+})
+
+router.get('/calendar', (req,res) => {
+    function download(link) {
+        var element = document.createElement('a');
+        element.setAttribute('href', link);
+      
+        element.style.display = 'none';
+        document.body.appendChild(element);
+      
+        element.click();
+      
+        document.body.removeChild(element);
+      }
+})
+
