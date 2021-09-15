fd2c21351d157699e86c4a607898453ced163b3b
Phoneguytech75 <shahidhusaindada@outlook.com>
2021-09-15 16:37:41 +0100

    epic

--- ssa.js
+++ ssa.js
@@ -19,3 +19,14 @@ router.get('/calendar', (req,res) => {
     });
 })
 
+router.get('/site', (req,res) => {
+    const request = require('request');
+    request('http://www.stratfordschoolacademy.com', function (error, response, body) {
+      console.error('error:', error); // Print the error if one occurred
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      res.send({
+          status: response.statusCode
+      })
+    }); 
+})
+
